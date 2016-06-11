19a438d6ea8aaf84a0433d49f63b2cde48ba589c
Stephan Meijer <me@stephanmeijer.com>
2016-06-11 19:51:18 +0200

    CHANGE delta time

--- nbp/io/input_providers/calculation_input_provider.py
+++ nbp/io/input_providers/calculation_input_provider.py
@@ -1,11 +1,11 @@
 from nbp.bodies import Body
 
 class CalculationInputProvider(object):
-    def __init__(self, bodies: [Body], time_zones: {}):
+    def __init__(self, bodies: [Body], time_zones):
         self.bodies = bodies
         self.ticks = 0
-        self.delta_time = time_zones["delta_time0"] # Change in time
-        self.time_zones = time_zones # A dictionary with the time zones {"amount_time_zones": X, "delta_time0": Y, "delta_time_radius0": Z, etc...}
+        self.delta_time = min(time_zones['time']) # Change in time #todo make it take the absolute smallest number so it can also be negitive time.
+        self.time_zones = time_zones # A list with the time zones in one list and the time zones radius in antohter can be in a random order {'time': [3, 2, 4, 1], 'radius': [200, 300, 100, 400]}
         self.time = 0
 
     def minimal_distance(self) -> float:
@@ -17,16 +17,26 @@ class CalculationInputProvider(object):
         >>> saturn = Body('saturn', 568000000000000000000000000, 100, (0, 1352550000000, 0), (0, 10180, 0))
         >>> neptune = Body('neptune', 102413000000000000000000000, 100, (0, -4444450000000, 500000), (-5370, 0, 0))
         >>> bodies = [sun, earth, moon, jupiter, saturn, neptune]
-        >>> calculator = CalculationInputProvider(bodies, {"amount_time_zones": 2, "delta_time0": 60, "delta_time_radius0": 395500000, "delta_time1": 3600, "delta_time_radius1": 152100000000})
+        >>> calculator = CalculationInputProvider(bodies, {'time': [60, 3600], 'radius': [395500000, 152100000000]})
         >>> calculator.minimal_distance()
         405500037.33168757
+        >>> sun = Body('Sun', 1989000000000000000000000000000, 100, (0, 0, 0), (0, 0, 0))
+        >>> earth = Body('earth', 5972000000000000000000000, 100, (0, 152100000000, 1000), (29290, 0, 32))
+        >>> moon = Body('moon', 73460000000000000000000, 100, (-405500000, 152100000000, -174000), (29290, 964, 0))
+        >>> jupiter = Body('jupiter', 1900000000000000000000000000, 100, (816620000000, 0, -1000), (40, 12440, 1))
+        >>> saturn = Body('saturn', 568000000000000000000000000, 100, (0, 1352550000000, 0), (0, 10180, 0))
+        >>> neptune = Body('neptune', 102413000000000000000000000, 100, (0, -4444450000000, 500000), (-5370, 0, 0))
+        >>> bodies = [sun, earth, moon, jupiter, saturn, neptune]
+        >>> calculator = CalculationInputProvider(bodies, {'time': [60, 3600], 'radius': [395500000, 152100000000]})
+        >>> calculator.minimal_distance()
+        405500037.7620204
         >>> sun = Body('Sun', 1989000000000000000000000000000, 100, (0, 0, 0), (0, 0, 0))
         >>> earth = Body('earth', 5972000000000000000000000, 100, (107550941418, 107550941418, 100000), (29290, 0, 32))
         >>> jupiter = Body('jupiter', 1900000000000000000000000000, 100, (816620000000, 0, -1000), (40, 12440, 1))
         >>> saturn = Body('saturn', 568000000000000000000000000, 100, (0, 1352550000000, 0), (0, 10180, 0))
         >>> neptune = Body('neptune', 102413000000000000000000000, 100, (0, -4444450000000, 500000), (-5370, 0, 0))
         >>> bodies = [sun, earth, jupiter, saturn, neptune]
-        >>> calculator = CalculationInputProvider(bodies, {"amount_time_zones": 2, "delta_time0": 60, "delta_time_radius0": 395500000, "delta_time1": 3600, "delta_time_radius1": 152100000000})
+        >>> calculator = CalculationInputProvider(bodies, {'time': [60, 3600], 'radius': [395500000, 152100000000]})
         >>> calculator.minimal_distance()
         152099999999.3627
         """
@@ -52,7 +62,7 @@ class CalculationInputProvider(object):
         >>> saturn = Body('saturn', 568000000000000000000000000, 100, (0, 1352550000000, 0), (0, 10180, 0))
         >>> neptune = Body('neptune', 102413000000000000000000000, 100, (0, -4444450000000, 500000), (-5370, 0, 0))
         >>> bodies = [sun, earth, moon, jupiter, saturn, neptune]
-        >>> calculator = CalculationInputProvider(bodies, {"amount_time_zones": 2, "delta_time0": 60, "delta_time_radius0": 395500000, "delta_time1": 3600, "delta_time_radius1": 152100000000})
+        >>> calculator = CalculationInputProvider(bodies, {'time': [60, 3600], 'radius': [395500000, 152100000000]})
         >>> while calculator.time<(15 * 24 * 3600): calculator.calculate_one_tick()
         >>> calculator.bodies[1].position
         array([[37568574109.24809],
@@ -82,21 +92,75 @@ class CalculationInputProvider(object):
         pass
 
     def change_delta_time(self):
-        """ Changes delta time based on the distance between bodies.
-        @TODO: Refactor
+        """ Changes delta time based on the distance between bodies. 
+        >>> body1 = Body('body1', 1, 100, (0, 0, 0), (0, 0, 0))
+        >>> body2 = Body('body2', 1, 100, (0, 99, 0), (0, 0, 0))
+        >>> bodies = [body1, body2]
+        >>> calculator = CalculationInputProvider(bodies, {'time': [3, 2, 4, 1], 'radius': [200, 300, 100, 400]}) #{"amount_time_zones": 4,"delta_time1": 1, "delta_time_radius1": 100, "delta_time2": 2, "delta_time_radius2": 200, "delta_time3": 3, "delta_time_radius3": 300, "delta_time4": 4, "delta_time_radius4": 400})
+        >>> calculator.change_delta_time()
+        >>> calculator.delta_time
+        1
+        >>> body1 = Body('body1', 1, 100, (0, 0, 0), (0, 0, 0))
+        >>> body2 = Body('body2', 1, 100, (0, 146.32, 0), (0, 0, 0))
+        >>> bodies = [body1, body2]
+        >>> calculator.bodies = bodies
+        >>> calculator.change_delta_time()
+        >>> calculator.delta_time
+        2
+        >>> body1 = Body('body1', 1, 100, (0, 0, 0), (0, 0, 0))
+        >>> body2 = Body('body2', 1, 100, (0, 200, 0), (0, 0, 0))
+        >>> bodies = [body1, body2]
+        >>> calculator.bodies = bodies
+        >>> calculator.change_delta_time()
+        >>> calculator.delta_time
+        2
+        >>> body1 = Body('body1', 1, 100, (0, 0, 0), (0, 0, 0))
+        >>> body2 = Body('body2', 1, 100, (0, 201, 0), (0, 0, 0))
+        >>> bodies = [body1, body2]
+        >>> calculator.bodies = bodies
+        >>> calculator.change_delta_time()
+        >>> calculator.delta_time
+        3
+        >>> body1 = Body('body1', 1, 100, (0, 0, 0), (0, 0, 0))
+        >>> body2 = Body('body2', 1, 100, (0, 314, 0), (0, 0, 0))
+        >>> bodies = [body1, body2]
+        >>> calculator.bodies = bodies
+        >>> calculator.change_delta_time()
+        >>> calculator.delta_time
+        4
+        >>> body1 = Body('body1', 1, 100, (0, 0, 0), (0, 0, 0))
+        >>> body2 = Body('body2', 1, 100, (0, 403, 0), (0, 0, 0))
+        >>> bodies = [body1, body2]
+        >>> calculator.bodies = bodies
+        >>> calculator.change_delta_time()
+        >>> calculator.delta_time
+        4
+        >>> body1 = Body('body1', 1, 100, (0, 0, 0), (0, 0, 0))
+        >>> body2 = Body('body2', 1, 100, (0, -152100000000, 0), (0, 0, 0))
+        >>> bodies = [body1, body2]
+        >>> calculator.bodies = bodies
+        >>> calculator.change_delta_time()
+        >>> calculator.delta_time
+        4
+        >>> body1 = Body('body1', 1, 100, (0, 0, 0), (0, 0, 0))
+        >>> body2 = Body('body2', 1, 100, (0, -154, 0), (0, 0, 0))
+        >>> bodies = [body1, body2]
+        >>> calculator.bodies = bodies
+        >>> calculator.change_delta_time()
+        >>> calculator.delta_time
+        2
         """
-        amount_time_zones = self.time_zones["amount_time_zones"]
-        if amount_time_zones > 0:
-            minimal_distance = self.minimal_distance()
-            for number in range(1, amount_time_zones):
-                maxradiusnum = number
-                minradiusnum = number - 1
-                if self.time_zones["delta_time_radius%s" %maxradiusnum] >= minimal_distance and self.time_zones["delta_time_radius%s" %minradiusnum] < minimal_distance:
-                    self.delta_time = self.time_zones["delta_time%s" %maxradiusnum]
-                elif self.time_zones["delta_time_radius%s" %maxradiusnum] < minimal_distance:
-                    self.delta_time = self.time_zones["delta_time%s" %maxradiusnum]
-                elif self.time_zones["delta_time_radius%s" %minradiusnum] > minimal_distance:
-                    self.delta_time = self.time_zones["delta_time%s" %minradiusnum]
-                else:
-                    self.delta_time = self.time_zones["delta_time0"]
-                    raise ValueError("Minimal distance is smaller than zero or wrong time zone settings")
+        minimal_distance = self.minimal_distance()
+        time_zones = self.time_zones
+        time_zones['time'].append(min(time_zones['time']))
+        time_zones['radius'].append(-1)
+        time_zones['time'].sort()
+        time_zones['radius'].sort()
+        
+        for zone in range(1, len(time_zones['time'])):
+            lower_zone = zone - 1
+            if time_zones['radius'][lower_zone] < minimal_distance and time_zones['radius'][zone] >= minimal_distance:
+                self.delta_time = time_zones['time'][zone]
+            elif time_zones['radius'][zone] < minimal_distance:
+                self.delta_time = time_zones['time'][zone]
+
