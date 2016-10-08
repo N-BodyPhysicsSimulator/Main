c962f519e844276984f634032b8a9b56b51feaf1
Stephan Meijer <me@stephanmeijer.com>
2016-10-08 15:43:37 +0200

    FIX failing test

--- tests/nbp/helpers/test_validation.py
+++ tests/nbp/helpers/test_validation.py
@@ -3,23 +3,23 @@ from __future__ import print_function
 from argparse import ArgumentTypeError
 
 import pytest
+import http.server
+import socketserver
+import threading
 import socket
 
 from nbp.helpers.validation import int_is_valid_port, int_is_open_port
 
 def test_closed_port(capsys):
-    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serversocket.bind((socket.gethostname(), 0))
-    serversocket.listen(5)
-    host, port = serversocket.getsockname()
+    httpd = socketserver.TCPServer(("", 0), http.server.SimpleHTTPRequestHandler)
+    host, port = httpd.socket.getsockname()
+    threading.Thread(target=httpd.serve_forever, args=(), daemon=True).start()
 
     with pytest.raises(ArgumentTypeError):
         assert int_is_open_port(port) != port
 
     with pytest.raises(ArgumentTypeError):
         assert int_is_valid_port(port) != port
-
-    serversocket.close()
 
 def test_open_port():
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
