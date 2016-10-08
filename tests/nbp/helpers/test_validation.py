e5d47741b6807e93909ad43191d99f8c69fffe9c
Stephan Meijer <me@stephanmeijer.com>
2016-10-08 15:05:12 +0200

    ADD tests for validation of port open/closed

---
+++ tests/nbp/helpers/test_validation.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+
+import pytest
+import socket
+
+from nbp.helpers.validation import int_is_valid_port, int_is_open_port
+
+def test_closed_port(capsys):
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversocket.bind((socket.gethostname(), 0))
+    serversocket.listen(5)
+    host, port = serversocket.getsockname()
+
+    with pytest.raises(ArgumentTypeError):
+        assert int_is_open_port(port) != port
+
+    with pytest.raises(ArgumentTypeError):
+        assert int_is_valid_port(port) != port
+
+def test_closed_port():
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversocket.bind((socket.gethostname(), 0))
+    host, port = serversocket.getsockname()
+
+    assert int_is_valid_port(port) == port
+    assert int_is_valid_port(port) == port
