54d5e0993c2aa50c309b5ee7e9aa24884faeac05
Stephan Meijer <me@stephanmeijer.com>
2016-10-10 14:33:19 +0200

    Improved test

--- tests/nbp/io/output_writers/test_json_input_provider_output_writer.py
+++ tests/nbp/io/output_writers/test_json_input_provider_output_writer.py
@@ -44,21 +44,21 @@ def test_json_input_provider_output_writer():
                 'name': 'sun',
                 'mass': 1234.56,
                 'radius': 200.0,
-                'position': { 'x': 58580.0, 'y': 15209.0, 'z': 16400.0 },
+                'position': { 'x': i * 58580.0, 'y': 15209.0, 'z': 16400.0 },
                 'velocity': { 'x': 29290.0, 'y': -11476.0, 'z': 320.0 }
             },
             {
                 'name': 'star',
                 'mass': 98765.0 / (i + 36),
                 'radius': 17574.0,
-                'position': { 'x': 15209.0, 'y': 29200.0, 'z': 2929.0 },
+                'position': { 'x': 15209.0, 'y': 29200.0 * 3 * i, 'z': 2929.0 },
                 'velocity': { 'x': -34428.0, 'y': 3200.0, 'z': 123.0 },
             }
         ],
         'ticks': 0,
         'time': 0.0,
         'delta_time': 10.0
-    }) for i in range(0, 3))
+    }) for i in range(1, 4))
 
     output_generator = get_simple_generator()
     JSONOutputWriter(args).handle(output_generator)
