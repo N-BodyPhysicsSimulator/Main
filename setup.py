f85fe17aa20912d4d74cd74ebd3a3bc627ac663b
Stephan Meijer <me@stephanmeijer.com>
2016-10-11 11:46:07 +0200

    Possible fix

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='nbp',
+      version='0.1.0',
+      description='N-Body Physics Simulator',
+      author='Stephan Meijer',
+      author_email='me@stephanmeijer.com',
+      url='https://github.com/N-BodyPhysicsSimulator/Main',
+      license='GPL-3.0',
+      packages=['nbp']
+     )
