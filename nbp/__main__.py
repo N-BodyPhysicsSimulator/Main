f03988eddfc2725207612bd4ab80a6f3ae71d23c
Stephan Meijer <me@stephanmeijer.com>
2016-10-11 11:43:37 +0200

    Create module-functionality for simulator

--- nbp/__main__.py
+++ nbp/__main__.py
@@ -1,7 +1,7 @@
 if __name__ != "__main__":
     print("File should be executed")
 else:
-    from nbp import Cli
+    from . import Cli
     from sys import argv
 
     Cli(argv[1:]).start_application()
