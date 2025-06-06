e06cdf1bc78aaf76f28c086afdf5b0891a8cb6ae
Stephan Meijer <me@stephanmeijer.com>
2025-06-06 08:39:13 +0200

    Use setuptools with dependencies

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='nbp',
-      version='0.1.0',
-      description='N-Body Physics Simulator',
-      author='Stephan Meijer',
-      author_email='me@stephanmeijer.com',
-      url='https://github.com/N-BodyPhysicsSimulator/Main',
-      license='GPL-3.0',
-      packages=['nbp']
-     )
+setup(
+    name='nbp',
+    version='0.1.0',
+    description='N-Body Physics Simulator',
+    author='Stephan Meijer',
+    author_email='me@stephanmeijer.com',
+    url='https://github.com/N-BodyPhysicsSimulator/Main',
+    license='GPL-3.0',
+    packages=['nbp'],
+    install_requires=[
+        'numpy',
+        'websockets'
+    ],
+)
