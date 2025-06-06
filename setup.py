2d52874f8be82d09141600b97d25fbf3bde0b2d6
Stephan Meijer <me@stephanmeijer.com>
2025-06-06 09:48:47 +0200

    Include dependencies in setup

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='nbp',
-      version='0.1.0',
-      description='N-Body Physics Simulator',
-      author='Stephan Meijer',
-      author_email='me@stephanmeijer.com',
-      url='https://github.com/N-BodyPhysicsSimulator/Main',
-      license='GPL-3.0',
-      packages=['nbp']
-     )
+setup(
+    name='nbp',
+    version='0.1.0',
+    description='N-Body Physics Simulator',
+    author='Stephan Meijer',
+    author_email='me@stephanmeijer.com',
+    url='https://github.com/N-BodyPhysicsSimulator/Main',
+    license='GPL-3.0',
+    packages=['nbp'],
+    install_requires=[
+        'gevent',
+        'bottle',
+        'websockets',
+        'numpy',
+        'cerberus',
+    ],
+)
